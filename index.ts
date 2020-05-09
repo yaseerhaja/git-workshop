80cf8916a1e5ca51a80aa1c64a2fd5fd2faddae5
kel-sakal-biyik <ugurkorfali@gmail.com>
2020-05-09 14:00:46 +0200

    use helper functions

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
+import { convertToAnchor, convertToParagraph } from "./helpers";
+
 // This function awesomely returns what it gets
 function myAwesomeFunction(argument: string): string {
-  return argument;
+  const url = "https://www.google.com";
+  return convertToParagraph(convertToAnchor(argument, url));
 }
