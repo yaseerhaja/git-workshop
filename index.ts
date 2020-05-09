33f33bc849e48f6407c065a2e5ed7ca39f181b1e
kel-sakal-biyik <ugurkorfali@gmail.com>
2020-05-09 13:27:46 +0200

    added paragraph converter

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 // This function awesomely returns what it gets
 function composeHTML(argument: string): string {
-  return convertToAnchor(argument, "https://www.altavista.com");
+  return convertToParagraph(
+    convertToAnchor(argument, "https://www.altavista.com")
+  );
 }
 
 function convertToAnchor(argument: string, url: string): string {
   return `<a href="${url}">${argument}</a>`;
+}
+
+function convertToParagraph(argument: string): string {
+  return `<p>${argument}</p>`;
 }
